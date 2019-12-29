eff648fe81b3a5897342ae3afa9144df0bcf8857
Deepak0404 <deepaksaini.net@gmail.com>
2019-12-30 01:13:14 +0530

    Type of stylesheets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import NameList from './components/NameList';
+import Person from './components/Person';
+import StyleSheet from './components/Stylesheet'
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className="App">
-      <Greet></Greet>
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <Inline/>
+      {/* <StyleSheet primary={true}/> */}
+      {/* <NameList/> */}
+      {/* <Greet></Greet> */}
     </div>
   );
 }
--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,13 +1,37 @@
 import React from 'react'
+import Person from './Person'
 
 function NameList(){
     const names = ['Deepak', 'Vikas', 'Vinita'];
-    const person = names.map(name => <h2>{name}</h2>)
-    return(
-        <div>
-            {person}
-        </div>
-    )
+    // const persons = [
+    //     {
+    //         id: 1,
+    //         name: 'Deepak',
+    //         age: 30,
+    //         skill: 'React'
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Subhash',
+    //         age: 32,
+    //         skill: 'Angular'
+    //     },
+    //     {
+    //         id: 3,
+    //         name: 'Arun',
+    //         age: 34,
+    //         skill: 'Testing'
+    //     },
+    //     {
+    //         id: 4,
+    //         name: 'Rahul',
+    //         age: 28,
+    //         skill: 'Php'
+    //     }
+    // ]
+    const nameList = names.map((name,index) => <h2 key={index}>{index} {name}</h2>)
+    //const personList = persons.map(person => <Person key={person.id} person={person}/>)
+    return <div>{nameList}</div>
 }
 
 export default NameList
